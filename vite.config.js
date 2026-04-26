982c794b7710ab8bdcf013cfcbb36c7ccb97dc03
akodad1377-debug <akodad1377@gmail.com>
2026-04-26 15:20:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['icon-192.png', 'icon-512.png'],
+      includeAssets: ['megrelian-icon.svg'],
       manifest: {
         name: 'მარგალური',
         short_name: 'მარგალური',
@@ -19,14 +19,9 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: 'megrelian-icon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml'
           }
         ]
       },
